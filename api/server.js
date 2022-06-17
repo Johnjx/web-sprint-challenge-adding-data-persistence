8eb299d68961bafade84bc6ad81fa3c79d2f8b5c
John Ruddock <john.jxr@pm.me>
2022-06-17 12:20:09 -0400

    server up

--- api/server.js
+++ api/server.js
@@ -1,1 +1,17 @@
-// build your server here and require it from index.js
+const express = require('express');
+
+const server = express();
+server.use(express.json());
+
+server.use('*', (req, res) => {
+    res.status(404).json({ message: '404 Not Found'})
+})
+
+server.use((err, req, res, next) => { // eslint-disable-line
+    res.status(err.status || 500).json({
+        message: err.message || 'Internal Server Error',
+        stack: err.stack
+    });
+})  
+
+module.exports = server;
